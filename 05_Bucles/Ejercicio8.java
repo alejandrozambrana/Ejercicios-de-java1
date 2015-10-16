1df15703d8922bb52fed16ec7024c65461e4c6b8
alejandrozambrana <ale_1996_zn@hotmail.com>
2015-10-16 13:00:01 +0200

    Acutalizacion de los ejercicios

--- 05_Bucles/Ejercicio8.java
+++ 05_Bucles/Ejercicio8.java
@@ -1,35 +1,21 @@
 /**
- * Ejercicio 7 
- * Realiza el control de acceso a una caja fuerte. La combinación será un número
- * de 4 cifras. El programa nos pedirá la combinación para abrirla. Si no 
- * acertamos, se nos mostrará el mensaje “Lo siento, esa no es la combinación” 
- * y si acertamos se nos dirá “La caja fuerte se ha abierto satisfactoriamente”.
- * Tendremos cuatro oportunidades para abrir la caja fuerte.
+ * Ejercicio 8
+ * Muestra la tabla de multiplicar de un número introducido por teclado.
  * 
  * @author Alejandro Zambrana Naranjo
  */
 
-public class Ejercicio7 {
+public class Ejercicio8 {
   public static void main(String[] args) {
     
-    System.out.println("REALIZA EL CONTROL DE ACCESO A UNA CAJA FUERTE");
+    System.out.println("MUESTRA LA TABLA DE MULTIPLICAR");
       
-      int codigo; 
-      int intentos = 4;
-      
-      do {
-        System.out.println("Introduce la conbinacion de la caja fuerte(4 digitos): " + intentos + " intentos");
-        codigo = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca que tabla de multiplicar quieres ver: ");
+    int tabla = Integer.parseInt(System.console().readLine());
         
-        intentos--;
-        
-        if (codigo == 2596) {
-          System.out.println("Ha abierto la caja fuerte.");
-          break;
-        } else {
-          System.out.println("Lo siento, ha agotado las 4 oportunidades.");
-        }
-      } while(intentos > 0);
+    for (int i = 0; i <= 10; i++) {
+      System.out.println(tabla + " x " + i + " = " + tabla * i);
     }
   }
+}
 
